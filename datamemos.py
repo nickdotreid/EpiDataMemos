248d9c2275fb057e8d6515326dfe15db53bced70
Nick Reid <nickreid@nickreid.com>
2012-01-09 13:36:52 -0800

    routed charts to own directory

--- datamemos.py
+++ datamemos.py
@@ -3,5 +3,5 @@ from charts.views import charts_app
 from memos.views import memos_app
 
 app = Flask(__name__)
-app.register_blueprint(charts_app)
+app.register_blueprint(charts_app,url_prefix='/charts')
 app.register_blueprint(memos_app,url_prefix='/memos')
