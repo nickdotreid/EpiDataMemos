3cae4d9af82972b2ba35f379835332123276f93f
Nick Reid <nickreid@nickreid.com>
2012-01-09 15:28:34 -0800

    moved parsing function to seperate file

--- charts/views.py
+++ charts/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify, abort, redirect
 import os, csv, xlrd, glob
+from parse_data import *
 
 charts_app = Blueprint('charts_app', __name__, static_folder='static', template_folder='templates')
 
@@ -11,28 +12,6 @@ def get_chart(chart):
 	if not os.path.exists(filename):
 		return jsonify({})
 	return jsonify(parse_data_file(filename))
-	
-def parse_data_file(file_location):
-	data = {}
-	wb = xlrd.open_workbook(file_location)
-	sh = wb.sheet_by_index(0)
-	keys = []
-	raw_keys = sh.row_values(0)
-	for num in range(len(raw_keys)-1):
-		value = raw_keys[num+1]
-		if type(value) is float:
-			value = int(value)
-		keys.append(str(value))
-	for key in keys:
-		data[key] = {}
-	for rownum in range(sh.nrows-1):
-		row = sh.row_values(rownum+1)
-		for cellnum in range(len(row)-1):
-			value = row[cellnum+1]
-			if type(value) is float:
-				value = int(value)
-			data[keys[cellnum]][row[0]] = value
-	return data
 	
 @charts_app.route('/')
 def home_page():
