7f495a79a203b6736ebb70bfb83c545ae33e2179
Nick Reid <nickreid@nickreid.com>
2012-01-09 12:51:22 -0800

    added more correct data parsing

--- charts/views.py
+++ charts/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify, abort
-import os
+import os, csv, xlrd
 
 charts_app = Blueprint('sharts_app', __name__)
 
@@ -14,5 +14,27 @@ def get_chart(chart):
 	filename = charts_dir+chart
 	if not os.path.exists(filename):
 		return 'no file'
-	return filename
+	data = parse_data_file(filename)
+	return jsonify({'data':data})
 	
+def parse_data_file(file_location):
+	data = {}
+	wb = xlrd.open_workbook(file_location)
+	sh = wb.sheet_by_index(0)
+	keys = []
+	raw_keys = sh.row_values(0)
+	for num in range(len(raw_keys)-1):
+		value = raw_keys[num+1]
+		if type(value) is float:
+			value = int(value)
+		keys.append(str(value))
+	for key in keys:
+		data[key] = {}
+	for rownum in range(sh.nrows-1):
+		row = sh.row_values(rownum+1)
+		for cellnum in range(len(row)-1):
+			value = row[cellnum+1]
+			if type(value) is float:
+				value = int(value)
+			data[keys[cellnum]][row[0]] = value
+	return data
