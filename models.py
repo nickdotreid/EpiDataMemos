d9039355f544f0346caff97f6781e17f4373ca8d
Nick Reid <nickreid@nickreid.com>
2012-01-09 08:52:43 -0800

    added Tag class

--- models.py
+++ models.py
@@ -19,3 +19,14 @@ class Memo(Base):
 
 	def __repr__(self):
 		return '<Test %r>' % (self.id)
+		
+class Tag(Base):
+	__tablename__ = 'tags'
+	id = Column(Integer,primary_key=True)
+	text = Column(String(50))
+	
+	def __init__(self,text):
+		self.text = text
+	
+	def __repr__(self):
+		return '<Tag %r>' % (self.text)
