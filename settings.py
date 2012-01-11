5746e37850a7ed1c4d7e50aac45233a5f2241525
Nick Reid <nickreid@nickreid.com>
2012-01-11 08:06:09 -0800

    changed server settings

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+DATA_CHART_DIR = '/Users/nickreid/Documents/sfhiv/DataMemos/website/data/'
 DATABASE_URI = 'sqlite:////Users/nickreid/Documents/sfhiv/DataMemos/website/database.db'
 DEBUG = False
 SECRET_KEY = 'not secret'
