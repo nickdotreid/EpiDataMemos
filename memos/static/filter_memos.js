44b268e70052cc35eee3abca104f0cbcaf43a432
Nick Reid <nickreid@nickreid.com>
2012-01-12 17:40:21 -0800

    now reorders memos based on filter input

--- memos/static/filter_memos.js
+++ memos/static/filter_memos.js
@@ -19,13 +19,39 @@ function filter_memos(obj){
 	if(!obj['highlight']){
 		obj['highlight'] = $.address.parameter("highlight")
 	}
-	$("#memos .memo").hide().each(function(){
-		memo = $(this);
-		if(memo.data("filter") && memo.data("filter")==obj['filter']){
-			memo.show();
+	memos = $("#memos .memo");
+	if(memos.length<1){
+		return false;
+	}
+	highlight = obj['highlight'];
+	filter = obj["filter"];
+	sorted_memos = memos.sort(function(a,b){
+		a = $(a);
+		b = $(b);
+		if(highlight && a.data("hightlight")!=b.data("highlight")){
+			if(a.data("highlight") == highlight){
+				return true;
+			}
+			if(b.data("highlight") == highlight){
+				return false;
+			}
 		}
-		if(memo.data("highlight") && memo.data("highlight")==obj['highlight']){
-			memo.show();
+		if(filter && a.data("filter")!=b.data("filter")){
+			if(a.data("filter") == filter){
+				return true;
+			}
+			if(b.data("filter") == filter){
+				return false;
+			}
+		}
+		if(a.data("weight")!=b.data("weight")){
+			return a.data("weight")<b.data("weight");
+		}
+		if(a.data("id")!=b.data("id")){
+			return a.data("id")<b.data("id");
 		}
 	});
+	for(var i=0;i<sorted_memos.length;i++){
+		$("#memos .create").after(sorted_memos[i]);
+	}
 }
