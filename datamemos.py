eeff6979489f005ab705a34849e9f620590112fc
Nick Reid <nickreid@nickreid.com>
2012-01-13 08:11:16 -0800

    removed useless junk

--- datamemos.py
+++ datamemos.py
@@ -6,7 +6,5 @@ import os
 
 app = Flask(__name__)
 
-charts_dir = os.environ['SFHIV_DATAMEMOS_CHARTS_DIR']
-
 app.register_blueprint(memos_app,url_prefix='/memos')
 app.register_blueprint(charts_app,url_prefix='/charts')
