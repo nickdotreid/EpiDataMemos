84170c55152cf5b6945eb746d3cc69122a2e697b
Nick Reid <nickreid@nickreid.com>
2012-01-23 10:17:18 -0800

    created and moved data retrival/parsing functions into single package- making barchart template just formatting

--- charts/views.py
+++ charts/views.py
@@ -1,29 +1,14 @@
 from flask import Blueprint, render_template, jsonify, abort, redirect
 import os, csv, xlrd, glob
-from parse_data import *
 
 charts_app = Blueprint('charts_app', __name__, static_folder='static', template_folder='templates')
 
 charts_dir = os.environ['SFHIV_DATAMEMOS_CHARTS_DIR']
 
-@charts_app.route('/json/<chart>')
-def get_data(chart):
-	filename = charts_dir+chart
-	if not os.path.exists(filename):
-		return jsonify({})
-	return jsonify(parse_data_file(filename))
-	
-@charts_app.route('/')
-def index():
-	charts = []
-	for xls in glob.glob(os.path.join(charts_dir,'*.xls')):
-		charts.append(xls.replace(charts_dir,''))
-	return render_template("index.html",charts=charts,title="Charts")
-	 
 @charts_app.route('/<chart>')
 def view(chart):
 	filename = charts_dir+chart
 	if not os.path.exists(filename):
-		return redirect("/")
+		return render_template('index.html')
 	title = chart.replace("_"," ").replace(".xls","")
 	return render_template('chart.html',filename=chart,title=title)
---
+++ data_xls/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, jsonify, abort, redirect
+import os, glob
+from parse_data import *
+
+data_api = Blueprint('data_xls', __name__, static_folder='static', template_folder='templates')
+
+charts_dir = os.environ['SFHIV_DATAMEMOS_CHARTS_DIR']
+
+@charts_app.route('/')
+def index():
+	charts = []
+	for xls in glob.glob(os.path.join(charts_dir,'*.xls')):
+		charts.append(xls.replace(charts_dir,''))
+	return jsonify(charts=charts)
+
+@data_api.route('/<chart>')
+def get_data(chart):
+	filename = charts_dir+chart
+	if not os.path.exists(filename):
+		return jsonify({})
+	return jsonify(parse_data_file(filename))
