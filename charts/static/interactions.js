4c44fe776ddc431004d140e4439c45f20a6e8432
Nick Reid <nickreid@nickreid.com>
2012-01-27 08:12:34 -0800

    added focus on parent to .chart dblclick

--- charts/static/interactions.js
+++ charts/static/interactions.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 	$("#chart").delegate(".bar","dblclick",function(event){
+		if(event.target!=this){
+			return true;
+		}
 		$.address.parameter("filter",$(this).data("name"));
+	}).delegate(".chart","dblclick",function(event){
+		if(event.target!=this){
+			return true;
+		}
+		event = fill_in_values(event);
+		items = $(this).parents("#chart:first").data("data");
+		find_values(items[0],function(value,index,parent){
+			if(event.filter == index && parent){
+				$.address.parameter("filter",parent);
+			}
+		});
 	});
 });
