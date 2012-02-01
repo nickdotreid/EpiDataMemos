20d7b1034866b732abc710a3b9ab1e07c649f0f0
Nick Reid <nickreid@nickreid.com>
2012-02-01 08:57:59 -0800

    formatting numbers with commas

--- charts/static/utilites.js
+++ charts/static/utilites.js
@@ -36,7 +36,19 @@ function format_number(num,percent){
 	if(percent){
 		return Math.round(num*100)+'%';
 	}
-	return Math.round(num);
+	return addCommas(Math.round(num));
+}
+
+function addCommas(nStr){
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
 }
 
 function in_array(arr,value){
