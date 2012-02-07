2b53378e8dcd5f18a4c4ea93c406f899ef65fc78
Nick Reid <nickreid@nickreid.com>
2012-02-07 07:55:10 -0800

    moved highlight functions to highlight.js

--- charts/static/bar.js
+++ charts/static/bar.js
@@ -73,45 +73,5 @@ $(document).ready(function(){
 //				$(this).css("z-index",$(this).data("z-index"));
 			}
 		});
-	}).delegate(".bar","highlight",function(event){
-		bar = $(this);
-		if(bar.data("highlight")){
-			bar.data("highlight").remove();
-		}
-		column = bar.parents(".column:first");
-		bar.trigger("format");
-		highlight = $(".highlight",bar).clone();
-		bar.data("highlight",highlight);
-		canvas = bar.parents(".canvas:first");
-		canvas.append(highlight);
-		graph = bar.parents(".chart:first");
-		var column_left = 0;
-		if(column.data("_left")){
-			column_left = column.data("_left");
-		}
-		x = column_left+bar.data("_left")+bar.width();
-		if(x+highlight.width()>graph.width()){
-			x = column_left+bar.data("_left")-highlight.width();
-			highlight.addClass("left");
-		}
-//		x = column.position().left+bar.data("_left")+(bar.width()/2)-(highlight.width()/2);
-		y = bar.data("_top")-(highlight.height()/2);
-		if(!y || y<0-Number(graph.css("padding-top").replace("px",""))){
-			y = 0-Number(graph.css("padding-top").replace("px",""));
-		}
-		
-		highlight.css({
-			top:y+'px',
-			left:x+'px'
-		})
-	}).delegate(".bar","unhighlight",function(event){
-		bar = $(this);
-		column = bar.parents(".column:first");
-		if($.address.parameter("filter")!=bar.data("name") || $.address.parameter("highlight")!= column.data("name")){
-			if(bar.data("highlight")){
-				bar.data("highlight").remove();
-			}
-			bar.data("highlight",false);
-		}
 	});
 });
