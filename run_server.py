fcd518c106496455dbeb07d1d9184c3b865a35af
Nick Reid <nickreid@nickreid.com>
2012-02-13 10:35:59 -0800

    added os.environ declaration

--- run_server.py
+++ run_server.py
@@ -1,2 +1,7 @@
+import os
+
+os.environ['SFHIV_DATAMEMOS_CHARTS_DIR'] = 'data/'
+os.environ['SFHIV_DATAMEMOS_DATABASE_URI'] = 'sqlite:////Users/nickreid/Documents/sfhiv/DataMemos/website/database.db'
+
 from datamemos import app
 app.run(host='0.0.0.0',debug=True)
