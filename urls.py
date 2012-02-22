341f27466418c95c964d88aa60ef304e525ab9e6
Nick Reid <nickreid@nickreid.com>
2012-02-22 09:32:03 -0800

    added base django files

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^charts/', include('charts.urls')),
+	url(r'^xls/', include('data_xls.urls')),
+	url(r'^notes/', include('notes.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
