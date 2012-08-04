2c31f2813c7aba5435218c9b96293fac38fa7118
Nick Reid <nickreid@nickreid.com>
2012-08-03 17:29:04 -0700

    styled table

--- datamemos/charts/static/charts/js/table.js
+++ datamemos/charts/static/charts/js/table.js
@@ -6,7 +6,39 @@ if(!statistic_get_uri){
 }
 
 $(document).ready(function(){
-	$(document).delegate(".chart .table .point","click",function(event){
+	$(".wrapper").delegate(".chart .table .point","click",function(event){
 		$.address.parameter("tags",$(this).data("tags"));
+	}).delegate(".chart .table .legend","click",function(event){
+		$.address.parameter("tags",$(this).data("tags"));
+	}).delegate(".chart .table","highlight",function(event){
+		var table = $(this);
+		$(".point.highlight,.legend.highlight",table).removeClass("highlight").removeClass("selected");
+		$(".point,.legend",table).each(function(){
+			var tags = $(this).data("tags");
+			if(!tags){
+				return;
+			}
+			tags = String(tags).split(",");
+			var matches = 0;
+			for(var index in tags){
+				if( event.tags.indexOf(tags[index])>-1){
+					matches++;
+				}
+			}
+			if(matches > 0){
+				$(this).addClass("highlight");
+			}
+			if(matches > 1){
+				$(this).addClass("selected");
+			}
+		});
+	});
+	
+	$.address.change(function(event){
+		$(".chart .table").trigger({
+			type:"highlight",
+			tags:$.address.parameter("tags").split(","),
+		});
+		
 	});
 });
