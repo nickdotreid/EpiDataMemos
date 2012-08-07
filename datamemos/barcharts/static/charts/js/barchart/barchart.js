bae565a6cace5c4677e3876b4b3a649f2fca0417
Nick Reid <nickreid@nickreid.com>
2012-08-06 18:23:57 -0700

    added redraw delay

--- datamemos/barcharts/static/charts/js/barchart/barchart.js
+++ datamemos/barcharts/static/charts/js/barchart/barchart.js
@@ -26,142 +26,145 @@ $(document).ready(function(){
 			$(this).data("percent",false);
 		}
 	}).delegate(".chart.barchart","redraw",function(event){
-		
-		// redraw delay
-		
 		var chart = $(this);
-		var canvas = $(".canvas",this);
-		var stacked = false;
+		if(chart.data("redraw-timeout")){
+			clearTimeout(chart.data("redraw-timeout"));
+		}
+		var timeout = setTimeout(function(){
+			var canvas = $(".canvas",chart);
+			var stacked = false;
 		
-		if(!event.tags){
-			event.tags = chart.data("tags");
-		}
-		if(event.percent == undefined){
-			event.percent = chart.data("percent");
-		}
-		var sibling_tags = [];
-		for(var index in event.tags){
-			var tag = event.tags[index];
-			$(".tags-children[parent='"+tag+"'] .tag input").each(function(){
-				event.tags.push(this.value);
-				stacked = true;
-			});
-			$(".tag input:not([value='"+tag+"'])",$(".tag input[value='"+tag+"']",chart).parents(".tags-children:first")).each(function(){
-				sibling_tags.push(this.value);
-			});
-		}
-		$(".column",chart).each(function(){
-			var column = $(this);
-			var total = 0;
-			$(".bar",column).each(function(){
-				var bar = $(this);
-				matches = 0;
-				for(var index in event.tags){
-					if(in_array(bar.data("tags"),event.tags[index])){
+			if(!event.tags){
+				event.tags = chart.data("tags");
+			}
+			if(event.percent == undefined){
+				event.percent = chart.data("percent");
+			}
+			var sibling_tags = [];
+			for(var index in event.tags){
+				var tag = event.tags[index];
+				$(".tags-children[parent='"+tag+"'] .tag input").each(function(){
+					event.tags.push(this.value);
+					stacked = true;
+				});
+				$(".tag input:not([value='"+tag+"'])",$(".tag input[value='"+tag+"']",chart).parents(".tags-children:first")).each(function(){
+					sibling_tags.push(this.value);
+				});
+			}
+			$(".column",chart).each(function(){
+				var column = $(this);
+				var total = 0;
+				$(".bar",column).each(function(){
+					var bar = $(this);
+					matches = 0;
+					for(var index in event.tags){
+						if(in_array(bar.data("tags"),event.tags[index])){
+							total += bar.data("amount");
+							matches++;
+						}
+					}
+					for(var index in sibling_tags){
+						if(in_array(bar.data("tags"),sibling_tags[index])){
+							matches++;
+						}
+					}
+					if(matches>0){
 						total += bar.data("amount");
-						matches++;
+						bar.data("matches",matches);
 					}
-				}
-				for(var index in sibling_tags){
-					if(in_array(bar.data("tags"),sibling_tags[index])){
-						matches++;
+				});
+				column.data("amount",total);
+			});
+		
+		
+			var chart_max = 0;
+			$(".column",chart).each(function(){
+				var column = $(this);
+				var biggest_number = 0;
+				var biggest_match = 1;
+				$(".bar",column).each(function(){
+					var bar = $(this);
+					var matches = 0;
+					for(var index in event.tags){
+						if(in_array(bar.data("tags"),event.tags[index])){
+							matches++;
+						}
 					}
-				}
-				if(matches>0){
-					total += bar.data("amount");
 					bar.data("matches",matches);
+					if(matches > biggest_match){
+						biggest_number = 0;
+						biggest_match = matches;
+					}
+					if(matches >= biggest_match){
+						biggest_number += bar.data("amount");
+					}
+				})
+				if(biggest_number>chart_max){
+					chart_max = biggest_number;
 				}
 			});
-			column.data("amount",total);
-		});
-		
-		
-		var chart_max = 0;
-		$(".column",chart).each(function(){
-			var column = $(this);
-			var biggest_number = 0;
-			var biggest_match = 1;
-			$(".bar",column).each(function(){
+			$(".bar").each(function(){
 				var bar = $(this);
-				var matches = 0;
-				for(var index in event.tags){
-					if(in_array(bar.data("tags"),event.tags[index])){
-						matches++;
+				var match = bar.data("matches");
+				var _height = 0;
+				var _y = 0;
+				var _x = 0;
+				var prev = bar.prev(".bar");
+				if(prev.length>0){
+					_x = prev.data("x");
+					if(!stacked && prev.data("height")>0){
+						var offset = bar.width()/10;
+						_x += offset;	
+					}
+					if(stacked){
+						_y = prev.data("y") + prev.data("height");	
 					}
 				}
-				bar.data("matches",matches);
-				if(matches > biggest_match){
-					biggest_number = 0;
-					biggest_match = matches;
+				if(match>0){
+					var value = 0;
+					if(event.percent){
+						value = bar.data("amount")/bar.parents(".column:first").data("amount");
+					}else{
+						value = bar.data("amount")/chart_max;
+					}
+					_height = canvas.height()*value;
 				}
-				if(matches >= biggest_match){
-					biggest_number += bar.data("amount");
-				}
-			})
-			if(biggest_number>chart_max){
-				chart_max = biggest_number;
-			}
-		});
-		$(".bar").each(function(){
-			var bar = $(this);
-			var match = bar.data("matches");
-			var _height = 0;
-			var _y = 0;
-			var _x = 0;
-			var prev = bar.prev(".bar");
-			if(prev.length>0){
-				_x = prev.data("x");
-				if(!stacked && prev.data("height")>0){
-					var offset = bar.width()/10;
-					_x += offset;	
-				}
-				if(stacked){
-					_y = prev.data("y") + prev.data("height");	
-				}
-			}
-			if(match>0){
-				var value = 0;
-				if(event.percent){
-					value = bar.data("amount")/bar.parents(".column:first").data("amount");
-				}else{
-					value = bar.data("amount")/chart_max;
-				}
-				_height = canvas.height()*value;
-			}
-			bar.data({
-				"height":_height,
-				"x":_x,
-				"y":_y
+				bar.data({
+					"height":_height,
+					"x":_x,
+					"y":_y
+				});
 			});
-		});
-		$(".bar").each(function(){
-			var bar = $(this);
-			bar.animate({
-				height:bar.data("height")+'px',
-				top:(canvas.height()-bar.data("height")-bar.data("y"))+"px",
-				left:bar.data("x")+"px"
-			},{
-				duration:500
+			$(".bar").each(function(){
+				var bar = $(this);
+				bar.animate({
+					height:bar.data("height")+'px',
+					top:(canvas.height()-bar.data("height")-bar.data("y"))+"px",
+					left:bar.data("x")+"px"
+				},{
+					duration:500
+				})
 			})
-		})
-		$(".column",chart).each(function(){
-			var column = $(this);
-			var widest = 0;
-			$(".bar",column).each(function(){
-				var right_pos = $(this).width() + Number($(this).css("left").replace("px",""));
-				if(right_pos > widest){
-					widest = right_pos;
-				}
+			$(".column",chart).each(function(){
+				var column = $(this);
+				var widest = 0;
+				$(".bar",column).each(function(){
+					var right_pos = $(this).width() + Number($(this).css("left").replace("px",""));
+					if(right_pos > widest){
+						widest = right_pos;
+					}
+				});
+				column.width(widest);
 			});
-			column.width(widest);
-		});
-		$(".grid",$(this)).trigger({
-			type:"grid_redraw",
-			filter:event.filter,
-			highlight:event.highlight,
-			percent:event.percent,
-			chart_max:chart_max
-		});
+			$(".grid",$(this)).trigger({
+				type:"grid_redraw",
+				filter:event.filter,
+				highlight:event.highlight,
+				percent:event.percent,
+				chart_max:chart_max
+			});
+		},500);
+		chart.data("redraw-timeout",timeout);
 	});
 	$.address.change(function(event){
 		$(".chart.barchart").trigger("get-state-chart").trigger("redraw");
