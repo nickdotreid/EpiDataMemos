fcc33808fc5557483f336958250fa6e8500d46d7
Nick Reid <nickreid@nickreid.com>
2012-08-07 22:10:05 -0700

    split production and dev settings

---
+++ datamemos/settings_development.py
@@ -0,0 +1,10 @@
+import os
+import dj_database_url
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {'default': dj_database_url.config(default='sqlite:////' + os.getcwd() + '/database.db')}
+
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
---
+++ datamemos/settings_production.py
@@ -0,0 +1,15 @@
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+if 'secret_key' in os.environ:
+	SECRET_KEY = os.environ['secrect_key']
+
+if 'aws_static_url' in os.environ and 'aws_access_key' in os.environ and 'aws_secret_access_key' in os.environ and 'aws_bucket_name' in os.environ:
+	STATIC_ROOT = ''
+	STATIC_URL = os.environ['aws_static_url']
+	
+	STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+	DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+	AWS_ACCESS_KEY_ID = os.environ['aws_access_key']
+	AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
+	AWS_STORAGE_BUCKET_NAME = os.environ['aws_bucket_name']
