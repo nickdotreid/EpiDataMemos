e724bd3e070d1658e6d1fa55c2f72a83f16f6282
Nick Reid <nickreid@nickreid.com>
2012-08-07 17:41:57 -0700

    scale looks nice

--- datamemos/barcharts/static/charts/js/barchart/scale.js
+++ datamemos/barcharts/static/charts/js/barchart/scale.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 	$(".wrapper").delegate(".chart.barchart .scale","scale-draw",function(event){
+		var scale = $(this);
+		var graph = scale.parents(".canvas:first");
+		var chart = scale.parents(".chart:first");
+		
+		if(!scale.data("grid")){
+			scale.data("grid",$('<div class="grid"></div>').prependTo(graph));
+		}
+		var grid = scale.data("grid");
+		
+		grid.css({
+			left:scale.width()+'px',
+			width:(graph.width()-scale.width())+'px'
+		})
+		
+		event_obj = {
+			type:'tick-draw',
+			max:event.max,
+			ticks:event.ticks
+		};
+		
+		grid.trigger(event_obj);
+		scale.trigger(event_obj);
+		
+	}).delegate(".chart.barchart .scale,.chart.barchart .grid","tick-draw",function(event){
 		if(!event.max){
 			return;
 		}
@@ -11,7 +35,7 @@ $(document).ready(function(){
 		for(var i=0;i<ticks.length;i++){
 			tick = ticks[i];
 			if($(".tick[data-value='"+tick+"']",scale).length<1){
-				scale.append('<div class="tick" data-value="'+tick+'">'+format_number(tick,event.percent)+'</div>');
+				scale.append('<div class="tick" data-value="'+tick+'"><span class="label">'+format_number(tick)+'</span></div>');
 				$(".tick:last",scale).css("top",graph.height()+'px').css("opacity",0);
 			}
 		}
