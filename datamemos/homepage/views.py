eb5aeb2efcbefd8382213e154c83655077093c33
Nick Reid <nickreid@nickreid.com>
2012-08-10 14:53:49 -0700

    added all viewable category items to list

--- datamemos/homepage/views.py
+++ datamemos/homepage/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from charts.models import Chart
+from notes.models import Category
 
 import json
 
@@ -12,4 +13,5 @@ def show_home(request):
 def load_chart(request):
 	return render_to_response('homepage/chart.html',{
 		'charts':Chart.objects.all(),
+		'categories':Category.objects.filter(viewable=True).all(),
 	},context_instance=RequestContext(request))
