b446e74c2c0af02e67ad014fdfebc4a9b8f1d665
Nick Reid <nickreid@nickreid.com>
2012-08-13 10:13:56 -0700

    added basic hover

--- datamemos/barcharts/static/charts/js/barchart/highlight.js
+++ datamemos/barcharts/static/charts/js/barchart/highlight.js
@@ -1,46 +1,24 @@
 $(document).ready(function(){
-	$("#chart").delegate(".bar","highlight",function(event){
-		bar = $(this);
-		$(".highlight:not(.bar .highlight)").remove();
-		if(bar.data("highlight")){
-			bar.data("highlight").remove();
-		}
-		column = bar.parents(".column:first");
-//		bar.trigger("format");
+	$(".wrapper").delegate(".chart .bar","highlight",function(event){
+		var bar = $(this);
+		var highlight = $(".highlight",bar);	
+		var canvas = bar.parents(".canvas:first");
 		
-		highlight = $(".highlight",bar).clone();
-		bar.data("highlight",highlight);
-		canvas = bar.parents(".canvas:first");
-		canvas.append(highlight);
-		graph = bar.parents(".chart:first");
+		bar.addClass("hover");
 		
-		$(".amount",highlight).show()
-		
-		var column_left = 0;
-		if(column.data("_left")){
-			column_left = column.data("_left");
-		}
-		x = column_left+bar.data("_left")+bar.width();
-		if(x+highlight.width()>graph.width()){
-			x = column_left+bar.data("_left")-highlight.width();
-			highlight.addClass("left");
-		}
-//		x = column.position().left+bar.data("_left")+(bar.width()/2)-(highlight.width()/2);
-		y = bar.data("_top")-(highlight.height()/2);
-		if(!y || y<0-Number(graph.css("padding-top").replace("px",""))){
-			y = 0-Number(graph.css("padding-top").replace("px",""));
-		}
+		y = 0 - highlight.height() - Number(highlight.css("margin-bottom").replace("px",""));
+		x = 0 - highlight.width()/2 + bar.width()/2;
 		
 		highlight.css({
 			top:y+'px',
 			left:x+'px'
-		})
-	}).delegate(".bar","unhighlight",function(event){
-		bar = $(this);
-		column = bar.parents(".column:first");
-		if(bar.data("highlight")){
-			bar.data("highlight").remove();
-		}
-		bar.data("highlight",false);
+		}).show();
+	}).delegate(".wrapper .bar","unhighlight",function(event){
+		$(this).removeClass("hover");
+		$(".highlight",$(this)).hide();
+	}).delegate(".chart .bar","mouseenter",function(event){
+		$(this).trigger("highlight");
+	}).delegate(".chart .bar","mouseleave",function(event){
+		$(this).trigger("unhighlight");
 	});
 });
