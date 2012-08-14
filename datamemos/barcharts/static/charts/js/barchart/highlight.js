2ab0cfdcaf21aac46f1312cb60bc3d73972e6f6e
Nick Reid <nickreid@nickreid.com>
2012-08-13 18:57:09 -0700

    moved highlight outside of bar element

--- datamemos/barcharts/static/charts/js/barchart/highlight.js
+++ datamemos/barcharts/static/charts/js/barchart/highlight.js
@@ -1,22 +1,38 @@
 $(document).ready(function(){
 	$(".wrapper").delegate(".chart .bar","highlight",function(event){
 		var bar = $(this);
-		var highlight = $(".highlight",bar);	
+		var column = bar.parents(".column:first");
 		var canvas = bar.parents(".canvas:first");
+		
+		$(".highlight:not(.bar .highlight)",canvas).remove();
+		
+		var highlight = $(".highlight",bar).clone();
+		canvas.append(highlight);
+		
+		var x_offset = bar.position().left + column.position().left;
+		var y_offset = bar.position().top;
+		
+		var x = 0;
+		var y = 0;
 		
 		$("."+bar.attr("class").replace(" ","."),canvas).addClass("hover");
 		
-		var y = 0 - highlight.height() - Number(highlight.css("margin-bottom").replace("px",""));
-		var x = 0 - highlight.width()/2 + bar.width()/2;
-		
-		combinded_height = bar.height()+highlight.height()+Number(highlight.css("margin-bottom").replace("px",""));
-		
-		if(bar.hasClass("stacked") || combinded_height > canvas.height()){
-			if(bar.height()>highlight.height()){
-				x = bar.width();
-				y = bar.height()/2 - highlight.height()/2;
-			}
-		}
+		if(bar.hasClass("stacked")){
+			x = x_offset + bar.width();
+			y = y_offset + bar.height()/2 - highlight.height()/2;
+		}else{
+			y = y_offset - highlight.height() - Number(highlight.css("margin-bottom").replace("px",""));
+			x = x_offset - highlight.width()/2 + bar.width()/2;
+
+			combinded_height = bar.height()+highlight.height()+Number(highlight.css("margin-bottom").replace("px",""));
+
+			if(y < 0){
+				x = x_offset + bar.width();
+				y = canvas.height()/2 - highlight.height()/2;
+			}else{
+				highlight.addClass("above");
+			}	
+		}		
 		
 		highlight.css({
 			top:y+'px',
@@ -24,7 +40,7 @@ $(document).ready(function(){
 		}).show();
 	}).delegate(".wrapper .bar","unhighlight",function(event){
 		$(".bar.hover").removeClass("hover");
-		$(".highlight",$(this)).hide();
+		$(".highlight:not(.bar .highlight)",$(this).parents("canvas:first")).remove();
 	}).delegate(".chart .bar","mouseenter",function(event){
 		$(this).trigger("highlight");
 	}).delegate(".chart .bar","mouseleave",function(event){
--- datamemos/homepage/static/homepage/js/interactions.js
+++ datamemos/homepage/static/homepage/js/interactions.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	$(".wrapper").delegate(".barchart .column","mouseenter",function(){
 		var column = $(this);
 		column_values = [];
