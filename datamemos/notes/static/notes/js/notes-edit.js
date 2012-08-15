d6123c931264baae5de4bfe7981cf4fd5fc9452c
Nick Reid <nickreid@nickreid.com>
2012-08-15 09:04:20 -0700

    seperated and cleaned up notes javascript

---
+++ datamemos/notes/static/notes/js/notes-edit.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+	$("#notes-edit").delegate(".notes-nav a","click",function(event){
+		event.preventDefault();
+		var button = $(this);
+		$("li.active",button.parents(".notes-nav:first")).removeClass("active");
+		button.parents("li:first").addClass("active");
+	}).delegate(".notes-nav .create-note","click",function(event){
+		event.preventDefault();
+		var button = $(this);
+		if($("form.note.create").length>0){
+			return;
+		}
+		$.ajax({
+			url:note_create_uri,
+			type:"GET",
+			success:function(data){
+				if(data['form']){
+					$("#notes-edit .notes-container").html(data['form']);
+					$("#notes-edit .notes-container form").prepend('<a href="#" class="close">close</a>');
+				}
+			}
+		});
+	}).delegate(".notes-container .close","click",function(event){
+		event.preventDefault();
+		$("#notes-edit .notes-container").html("");
+		$("#notes-edit .notes-nav .active ").removeClass("active");
+	}).delegate("form.note.create","submit",function(event){
+		event.preventDefault();
+		var form = $(this);
+		form.addClass("loading");
+		form.trigger("presubmit");
+		$.ajax({
+			url:note_create_uri,
+			type:"POST",
+			data:form.serialize(),
+			success:function(data){
+				if(data['note']){
+					if(data['note']['type']){
+						$("#notes-list .notes-nav a[note-type='"+data['note']['type']+"']").click();
+					}
+					if(data['note']['markup']){
+						form.after(data['note']['markup']);	
+					}
+				}
+				form.remove();
+				$("#notes-edit .notes-nav .active").removeClass("active");
+
+			},
+			error:function(data){
+				if(data['form']){
+					form.after(data['form']);
+					form.remove();
+				}
+			}
+		});
+	});	
+});
--- datamemos/notes/static/notes/js/notes.js
+++ datamemos/notes/static/notes/js/notes.js
@@ -2,51 +2,8 @@ if(!note_create_uri){
 	var note_create_uri = "/notes/create/";
 }
 $(document).ready(function(){
-	$("#note-container").delegate(".create-note","click",function(event){
-		event.preventDefault();
-		var button = $(this);
-		if($("form.note.create").length>0){
-			return;
-		}
-		$.ajax({
-			url:note_create_uri,
-			type:"GET",
-			success:function(data){
-				if(data['form']){
-					$("#notes-edit-area").html(data['form']);
-					$("#notes-edit-area form").prepend('<a href="#" class="close">close</a>');
-					button.parents("li:first").addClass("active");
-				}
-			}
-		})
-	}).delegate("#notes-edit-area .close","click",function(event){
-		event.preventDefault();
-		$("#notes-edit-area").html("");
-		$("#note-container .create-note").parents("li:first").removeClass("active");
-	}).delegate("form.note.create","submit",function(event){
-		event.preventDefault();
-		var form = $(this);
-		form.addClass("loading");
-		form.trigger("presubmit");
-		$.ajax({
-			url:note_create_uri,
-			type:"POST",
-			data:form.serialize(),
-			success:function(data){
-				form.remove();
-				$("#note-container .create-note").parents("li:first").removeClass("active");
-				$("#note-type-selector a[note-type='comment']").click();	// should reload correct screen
-			},
-			error:function(data){
-				if(data['form']){
-					form.after(data['form']);
-					form.remove();
-				}
-			}
-		});
-	});
-	$("#note-container").delegate(".notes","sort",function(event){
-		var container = $(this);
+	$("#notes-list").bind("sort",function(event){
+		var container = $(".notes-container",$(this));
 		var notes = $(".note",container);
 		var ignore_values = [];
 		$(".chart .column").each(function(){
@@ -86,12 +43,12 @@ $(document).ready(function(){
 		for(var i=0;i<sorted_notes.length;i++){
 			container.append(sorted_notes[i]);
 		}
-	}).delegate(".share-statistic","click",function(event){
-		event.preventDefault;
 	});
-	$(".wrapper").delegate("#note-container","get-notes",function(event){
+	$("#notes-list").bind("get-notes",function(event){
+		var list = $(this);
+		var container = $(".notes-container",list);
 		noteType = false;
-		$("#note-type-selector li.active a").each(function(){
+		$(".notes-nav li.active a").each(function(){
 			noteType = $(this).attr("note-type");
 		})
 		$(".notes .note").remove();
@@ -111,18 +68,18 @@ $(document).ready(function(){
 							$(note['markup']).after(original);
 							original.remove();
 						}else{
-							$("#note-container .notes").append(note['markup']);
+							container.append(note['markup']);
 						}
 					}
-					$("#note-container .notes .note").trigger("notes-init");
-					$("#note-container .notes").trigger({
+					$(".note",container).trigger("notes-init");
+					list.trigger({
 						type:"sort",
 						tags:String($.address.parameter("tags")).split(",")
 					});
 				}
 			}
 		});
-	}).delegate("#note-container .notes .note","notes-init",function(event){
+	}).delegate(".notes-container .note","notes-init",function(event){
 		var note = $(this);
 		if(note.data("init")){
 			return;
@@ -136,23 +93,26 @@ $(document).ready(function(){
 			$(this).data("child-tags",$(this).attr("child-tags").split(","));
 		});
 	});
+	
 	$.address.change(function(event){
 		$("#note-container .notes").trigger({
 			type:"sort",
 			tags:String($.address.parameter("tags")).split(",")
 		});
 	});
-	$("#note-type-selector li a").click(function(event){
+	
+	$("#notes-list .notes-nav li a").click(function(event){
 		event.preventDefault();
 		var link = $(this);
-		$("li.active",link.parents("#note-type-selector:first")).removeClass("active");
+		$("li.active",link.parents(".notes-nav:first")).removeClass("active");
 		link.parents("li").addClass("active");
-		$("#note-container").trigger({
+		$("#notes-list").trigger({
 			type:"get-notes",
 			chart_id:$.address.parameter("chart")
-			})
-	})
-	$("#note-type-selector li:first").addClass("active");
+			});
+	});
+	
+	$("#notes-list .notes-nav li:first").addClass("active");
 });
 
 function arr_similar_count(arr1,arr2,ignore_arr){
