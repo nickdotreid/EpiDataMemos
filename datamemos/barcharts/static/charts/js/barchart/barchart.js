41c9d160e8e0a428591b786ee53d2e3175cc9793
Nick Reid <nickreid@nickreid.com>
2012-09-21 15:45:42 -0700

    tag controls are mostly working

--- datamemos/barcharts/static/charts/js/barchart/barchart.js
+++ datamemos/barcharts/static/charts/js/barchart/barchart.js
@@ -27,36 +27,37 @@ Chart = Backbone.Model.extend({
 	load: function(data){
 		this.set(data);
 		var chart = this;
-		var set_tag = function(tag){
-			return new Tag({
+
+		var rows = new TagCollection();
+		_(data['rows']).forEach(function(tag){
+			rows.add({
 				short: tag['short'],
 				name: tag['name'],
 				parent: tag['parent'],
 				children: tag['children'],
-				sibilings: tag['sibilings']
+				siblings: tag['sibilings']
 			});
-		}
-		this.rows = _(data['rows']).map(set_tag);
-		var rows = this.rows;
-		_(rows).forEach(function(tag){
-			tag.set_property('children',rows);
-			tag.set_property('sibilings',rows);
-			var parent = false;
-			_(rows).forEach(function(row){
-				if(tag.parent == row.get("short")){
-					parent = tag;
-				}
-			});
-			tag.set("parent",parent);
 		});
-		this.columns = _(data['columns']).map(set_tag);
+		rows.connect_tags();
+		this.set("rows",rows);
 		
-		this.points = _(data['points']).map(function(point){
+		var columns = new TagCollection();
+		_(data['columns']).forEach(function(tag){
+			columns.add({
+				short: tag['short'],
+				name: tag['name'],
+			});
+		});
+		this.set("columns",columns);
+		
+		var points = _(data['points']).map(function(point){
 			return new Point({
 				value:point['value'],
 				tags:point['tags']
 			});
 		});
+		this.set("points",points);
+		
 		this.trigger("loaded");
 	}
 });
@@ -76,27 +77,24 @@ ChartView = Backbone.View.extend({
 		this.$el = $(this.el);
 		
 		var pallet = this.pallet;
-		_(_(this.model.rows).filter(function(tag){
-			return !tag.parent;
-		})).forEach(function(tag){
+		this.model.get("rows").filter(function(tag){
+			return !tag.get("parent");
+		}).forEach(function(tag){
 			tag.set_color(pallet);
 		});
 		
 		// render control field sets for columns & rows
-		$('<fieldset class="control-rows"><div class="btn-group row tags-row tags"></div></fieldset>').appendTo(".controls",this.$el);		
-		var control_rows = $(".control-rows:first",this.$el);
-		var tags_row = $(".tags-row:first",control_rows);
-		_(_(this.model.rows).filter(function(tag){
-			return tag.get('children').length > 0;
-		})).forEach(function(row){
-			btn = new TagButton({
-				model:row,
-				fieldset:control_rows,
-				container:tags_row
-			});
-			btn.render();
-			// if children add views (maybe when initializing??)
+		rows_control = new TagButtonField({
+			collection:this.model.get("rows")
 		});
+		rows_control.render();
+		rows_control.$el.appendTo(".controls",this.$el);
+		
+		columns_control = new TagButtonField({
+			collection:this.model.get("columns")
+		});
+		columns_control.render();
+		columns_control.$el.appendTo(".controls",this.$el);	
 		
 		var canvas = $(".canvas",$(this.el));
 		this.paper = Raphael(canvas[0],canvas.width(),canvas.height());
@@ -109,9 +107,6 @@ ChartView = Backbone.View.extend({
 			point_view.render();
 		});
 		return this;
-	},
-	view: function(){
-		
 	}
 });
 
@@ -147,7 +142,14 @@ Tag = Backbone.Model.extend({
 		siblings:[],
 		parent:false,
 		short:"",
-		name:""
+		name:"",
+		color:false,
+		selected:false
+	},
+	connect_to: function(items){
+		this.set_property('children',items);
+		this.set_property('siblings',items);
+		this.set_parent(items);
 	},
 	set_property: function(property,items){
 		var items = items;
@@ -162,6 +164,16 @@ Tag = Backbone.Model.extend({
 		this.set(property,_children);
 		return this;
 	},
+	set_parent: function(items){
+		var tag = this;
+		var parent = false;
+		_(items).forEach(function(row){
+			if(tag != row && tag.get('parent') == row.get("short")){
+				parent = tag;
+			}
+		});
+		tag.set("parent",parent);
+	},
 	set_color: function(pallet){
 		var pallet = pallet;
 		var children = this.get("children");
@@ -175,6 +187,58 @@ Tag = Backbone.Model.extend({
 	}
 });
 
+TagCollection = Backbone.Collection.extend({
+	model:Tag,
+	initialize: function(){
+		var collection = this;
+		this.on('change:selected',function(tag_selected){
+			if(tag_selected.get("selected")){
+				collection.without([tag_selected]).forEach(function(tag){
+					if(tag != tag_selected){
+						tag.set("selected",false);
+					}
+				});				
+			}
+		});
+	},
+	connect_tags: function(){
+		var items = this.models;
+		this.forEach(function(tag){
+			tag.connect_to(items);
+		});
+	}
+});
+
+TagButtonField = Backbone.View.extend({
+	initialize:function(options){
+		this['collection'] = options.collection;
+		this.template = _.template('<fieldset class="control-rows"></fieldset>');
+		this.row_template = _.template('<div class="btn-group row tags-row tags"></div>');
+	},
+	render:function(){
+		this.el = this.template({});
+		this.$el = $(this.el);
+		this.$el.prepend(this.render_row(this["collection"].filter(function(tag){
+			return !tag.get("parent");
+		})));
+		
+	},
+	render_row: function(tags){
+		var buttonField = this;
+		var tags_row = $(this.row_template({}));
+		tags.forEach(function(row){
+			btn = new TagButton({
+				model:row,
+				container:tags_row,
+				row_template:buttonField.row_template,
+				fieldset:buttonField.$el
+			});
+			btn.render();
+		});
+		return tags_row;
+	}
+})
+
 TagButton = Backbone.View.extend({
 	events:{
 		'click .btn':'selected'
@@ -183,13 +247,20 @@ TagButton = Backbone.View.extend({
 		this.template = _.template($("#tag-button-template").html());
 		this.container = options.container;
 		this.fieldset = options.fieldset;
+		this.row_template = options.row_template;
+		var btn = this;
+		this.model.bind("change:selected",function(){
+			btn.toggle();
+		});
 	},
 	render:function(){
 		this.$el.html(this.template(this.model.toJSON()))
 		this.$el.appendTo(this.container);
+		this.btn = $('.btn',this.$el);
 		if(this.model.get('children').length > 0){
-			this.child_row = $('<div class="btn-group tags tags-row row child-row"></div>').appendTo(this.fieldset);
-			var row = this.child_row;
+			var row = $(this.row_template({})).appendTo(this.fieldset);
+			row.addClass("child-row");
+			this.child_row = row;
 			var fieldset = this.fieldset;
 			_(this.model.get('children')).forEach(function(tag){
 				var btn = new TagButton({
@@ -200,18 +271,30 @@ TagButton = Backbone.View.extend({
 				btn.render();
 			});
 		}
+		this.toggle();
 		return this;
 	},
 	selected:function(){
-		$(".btn.active",this.fieldset).removeClass("active");
-		$(".child-row",this.fieldset).hide();
-		
-		$('.btn',this.$el).addClass("active");
-		this.container.show();
-		if(this.child_row){
-			this.child_row.show();
+		if(this.model.get('selected')){
+			this.model.set("selected",false);
+		}else{
+			this.model.set("selected",true);
 		}
-		
-		this.model.trigger("selected");
+	},
+	toggle: function(){
+		if(this.model.get("selected")){
+			this.btn.addClass("active");
+			this.container.show();
+			if(this.child_row){
+				this.child_row.show();
+			}
+		}else{
+			this.btn.removeClass("active");
+			if(this.child_row && !_(this.model.get("children")).find(function(tag){
+				return tag.get("selected");
+			})){
+				this.child_row.hide();
+			}
+		}
 	}
 });
