024c1991119a873ace0917ad8528f1611faeb84c
Nick Reid <nickreid@nickreid.com>
2012-10-11 10:56:22 -0700

    removed messages - isnt working how i want

--- datamemos/homepage/static/homepage/js/messages.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(event){
-	$(document).ajaxComplete(function(event, XMLHttpRequest, ajaxOptions){
-		var data = $.parseJSON(XMLHttpRequest.responseText);
-		if(data && data['message']){
-			alert(data['message']['text']);
-		}
-	});
-});
