90b18b0fbd9d49645fa0d4092416370efbd4bdde
Nick Reid <nickreid@nickreid.com>
2012-10-22 10:25:28 -0700

    moved from bootstrap affix to my .sticky code

--- datamemos/static/datamemos/js/page.js
+++ datamemos/static/datamemos/js/page.js
@@ -5,20 +5,24 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$(".sticky").each(function(){
 		$(this).data("top",$(this).offset().top);
+		$(this).data("width",$(this).width());
 	});
 	$(window).scroll(function(event){
 		$(".sticky").each(function(){
-			var sticky = $(this)
-			if($(window).scrollTop() > sticky.data("top") && sticky.width() != sticky.parents(".container").width()){
-				sticky.css({
+			var sticky = $(this);
+			if($(window).scrollTop() > sticky.data("top")){
+				sticky.addClass("sticky-stuck").css({
 					position:'fixed',
-					top:'0px'
+					top:'0px',
+					width: sticky.data("width")
 				});
+				// catch the bottom??
 			}else{
-				sticky.css({
+				sticky.removeClass("sticky-stuck").css({
 					position:'static',
-					top:'0px'
-				});				
+					top:'0px',
+					width:"auto"
+				});
 			}
 		});
 	});
