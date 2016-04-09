03d46d2dfd420969c2954edb8340c2160f15f4f2
Edward Betts <edward@4angle.com>
2016-04-09 17:33:48 +0100

    make code more pythonic

--- datamemos/barcharts/templatetags/as_percent.py
+++ datamemos/barcharts/templatetags/as_percent.py
@@ -3,8 +3,6 @@ from django import template
 register = template.Library()
 
 def as_percent(value):
-	if not isinstance(value,float):
-		return ""
-	return "{0:.2f}%".format(value*100)
+	return "{:.2%}".format(value) if isinstance(value, float) else "" 
 
 register.filter('as_percent', as_percent)
